3748ea1f9fea75e9f53330c6d6fdc859e867ac8b
Victor Freitag <132359912+victorfreitag@users.noreply.github.com>
2024-05-07 16:20:12 -0300

    face

---
+++ src/app/index.js
@@ -0,0 +1,29 @@
+import { FlatList, View, Text } from "react-native";
+import React from "react";
+import { Redirect } from "expo-router";
+
+const data = [
+  {id:'1', title: 'Item 1'},
+  {id:'2', title: 'Item 2'},
+  {id:'3', title: 'Item 3'},
+  {id:'4', title: 'Item 4'},
+  {id:'5', title: 'Item 5'},
+
+];
+
+  export default function listarSimples(){
+    const renderItem = ({item}) => (
+      <View style={{padding:10, borderBottomWidth:1, borderBottomColor: '#ccc'}}>
+        <Text>{item.title}</Text>
+      </View>
+    );
+
+    return(
+      <FlatList 
+      horizontal
+      data={data}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}/>
+    );
+  }
+
